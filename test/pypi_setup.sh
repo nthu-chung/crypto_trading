a91b11daa7af991cd9fdd068ce2bdd9cc3b53951
Harvin W <harvin.w@binance.com>
2025-12-13 11:59:03 +0800

    update python packages test script

---
+++ test/pypi_setup.sh
@@ -0,0 +1,25 @@
+export TWINE_USERNAME='binance_agent'
+export TWINE_PASSWORD='pypi-your-token-here'
+
+
+
+# 1. 安装工具
+pip3 install --upgrade build twine
+
+# 2. 清理
+rm -rf build/ dist/ *.egg-info
+
+# 3. 构建
+python3 -m build
+
+# 4. 检查
+python3 -m twine check dist/*
+
+# 5. 上传到 TestPyPI（测试）
+python3 -m twine upload --repository testpypi dist/*
+
+# 6. 测试安装
+pip3 install -i https://test.pypi.org/simple/ cyqnt-trd
+
+# 7. 上传到正式 PyPI
+python3 -m twine upload dist/*
